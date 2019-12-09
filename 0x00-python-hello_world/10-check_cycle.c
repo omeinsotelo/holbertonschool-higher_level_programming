f5f54f7254859b1f0697f4e9e1d2e4a7de422201
omeinsotelo <omeinsotelo@gmail.com>
2019-12-09 22:46:55 +0000

    check fixed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *list)
 
 	list1 = list;
 	list2 = list;
-	while (list1 != NULL && list2 != NULL && list1->next != NULL)
+	while (list1 != NULL && list2 != NULL && list2->next != NULL)
 	{
 		list1 = list1->next;
 		list2 = list2->next->next;
