b965406a1cbcfca3029c48ab0c48e1099b2860ef
omeinsotelo <omeinsotelo@gmail.com>
2019-12-10 19:06:12 +0000

    fixed

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-	if i != 101 & i != 113:
-    		print(chr(i), end="")
+        if i != 113 and i != 101:
+                print("{:s}".format(chr(i)), end="")
