664fa4fbace4c5aeca4742b7628510975b8a481a
omeinsotelo <omeinsotelo@gmail.com>
2019-12-17 22:14:50 +0000

    add tuple fixed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,15 +2,17 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     var1 = 0
     var2 = 0
-    if len(tuple_a) == 2:
-        var1 += tuple_a[0]
-        var2 += tuple_a[1]
-    elif len(tuple_a) == 1:
-        var1 += tuple_a[0]
-    if len(tuple_b) == 2:
-        var1 += tuple_b[0]
-        var2 += tuple_b[1]
-    elif len(tuple_b) == 1:
-        var1 += tuple_b[0]
+    if tuple_a:
+        if len(tuple_a) == 2:
+            var1 += tuple_a[0]
+            var2 += tuple_a[1]
+        elif len(tuple_a) == 1:
+            var1 += tuple_a[0]
+    if tuple_b:
+        if len(tuple_b) == 2:
+            var1 += tuple_b[0]
+            var2 += tuple_b[1]
+        elif len(tuple_b) == 1:
+            var1 += tuple_b[0]
     tuple_add = (var1, var2)
     return tuple_add
