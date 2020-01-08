e10632471e905ded7555d577531eb9faeb344058
omeinsotelo <omeinsotelo@gmail.com>
2020-01-08 14:40:08 -0500

    safe print list integer fixed

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     j = 0
     for i in range(x):
         k = my_list[i]
-        try: 
+        try:
             print("{:d}".format(k), end="")
             j += 1
         except:
