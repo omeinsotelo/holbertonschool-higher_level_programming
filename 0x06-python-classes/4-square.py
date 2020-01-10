2a6fa9ad394324fe7e2ef044a928ab2950a17789
omeinsotelo <omeinsotelo@gmail.com>
2020-01-09 20:13:30 -0500

    squere 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+class Square:
+    """Empty class square"""
+
+    def __init__(self, size=0):
+        """init square function"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """Get the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, newSize):
+        """Get the size"""
+        if not isinstance(newSize, int):
+            raise TypeError("size must be an integer")
+        elif newSize < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = newSize
+
+    def area(self):
+        """Get the area"""
+        return self.__size * self.__size
