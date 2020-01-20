76274d07bb8131cafa2b3873798ed32b212e7886
omeinsotelo <omeinsotelo@gmail.com>
2020-01-20 18:32:34 -0500

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Class Square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square inherits from Rectangle """
+    def __init__(self, size):
+        """ initialize size """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ Return area """
+        return self.__size ** 2
