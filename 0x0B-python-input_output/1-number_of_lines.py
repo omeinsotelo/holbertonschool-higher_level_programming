004b501827680dcf3e5cf633c0c10cc3cd615425
omeinsotelo <omeinsotelo@gmail.com>
2020-01-21 14:25:57 -0500

    1-number_of_lines.py

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Returns the number of lines of a text file """
+
+
+def number_of_lines(filename=""):
+    """ Function returns number lines """
+    with open(filename, encoding='utf8') as file:
+        return len(file.read().split("\n"))
