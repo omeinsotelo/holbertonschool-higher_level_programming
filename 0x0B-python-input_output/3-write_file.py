9ce2ae0d6896eb7fe1557f08d73c2a52dc80bb85
omeinsotelo <omeinsotelo@gmail.com>
2020-01-21 17:47:45 -0500

    3-write_file.py

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """ Function writes a string return num char """
-    with open(filename, 'w', encoding="utf-8") as f:
-        return f.write(text)
+    with open(filename, 'w', encoding="utf-8") as file:
+        return file.write(text)
