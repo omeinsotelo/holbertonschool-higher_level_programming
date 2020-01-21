d1faa6bb3272147fb22b2d01f9b7554df5553883
omeinsotelo <omeinsotelo@gmail.com>
2020-01-21 17:59:28 -0500

    5-to_json_string.py

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+""" Returns the JSON representation of an object """
+
+
+def to_json_string(my_obj):
+    """ Function returns JSON repres object """
+    return json.dumps(my_obj)
