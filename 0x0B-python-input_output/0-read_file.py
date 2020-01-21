c499312d514e3c6ab1fa438594f23b741e16c6bc
omeinsotelo <omeinsotelo@gmail.com>
2020-01-21 17:17:29 -0500

    0-read_file.py fixing

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Reads a text file """
+"""Reads a text file aun print"""
 
 
 def read_file(filename=""):
-    """ Function that reads a text file """
-    with open(filename, encoding='utf8') as f:
+    """Function read and print"""
+    with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
