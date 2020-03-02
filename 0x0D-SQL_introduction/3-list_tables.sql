1029ab76698e3ddd2872aa440f22522825629c1d
omeinsotelo <omeinsotelo@gmail.com>
2020-03-02 13:49:46 -0500

    update 0-6

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- List tables
+-- Script that lists all the tables of a database in your MySQL server.
+SHOW TABLES 
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Full description
+-- Script that prints the full description of the table 
+SWOW CREATE TABLE first_table
