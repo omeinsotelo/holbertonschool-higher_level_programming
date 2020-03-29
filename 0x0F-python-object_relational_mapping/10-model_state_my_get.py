5eb453da311a73edcea9670e3a942fafa05e755e
omeinsotelo <omeinsotelo@gmail.com>
2020-03-28 21:13:31 -0500

    apload 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# list all states
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
+from sqlalchemy import func
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+            pool_pre_ping=True
+        )
+    Base.metadata.create_all(engine)
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
+    # get the data - there are objects
+    records = session.query(State).filter_by(name=sys.argv[4]).all()
+    if not records:
+            print("Not found")
+    for row in records:
+        print("{}".format(row.id))
+    # close session
+    session.close()
