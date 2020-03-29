02b6eab5cd99006d7763eb8a3aa63f3f77d98af0
omeinsotelo <omeinsotelo@gmail.com>
2020-03-28 21:19:08 -0500

    apload 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# list all states
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
+from sqlalchemy import func
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+            pool_pre_ping=True
+        )
+    Base.metadata.create_all(engine)
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
+    # get the data - there are objects
+    _filter = State.name.like(func.binary('%a%'))
+    session.query(State).filter(_filter).delete(synchronize_session='fetch')
+    # confirm the change
+    session.commit()
+    # close session
+    session.close()
