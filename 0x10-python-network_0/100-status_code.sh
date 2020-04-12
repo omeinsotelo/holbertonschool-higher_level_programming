14880a74b0c33e1bba085b723881f4e486b96371
omeinsotelo <omeinsotelo@gmail.com>
2020-04-12 12:52:21 -0500

    fixing 100 and 101

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a request to a URL passed as an argument
-# and displays only the status code of the response.
+# Sends a request to a URL, displays only status code of response.
 curl -s "$1" -o /dev/null -w '%{http_code}'
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument
-# and displays the body of the response.
+# Sends a JSON POST request to a URL passed, displays body of response.
 curl -sX POST -H "Content-type: application/json" -d @"$2" "$1"
