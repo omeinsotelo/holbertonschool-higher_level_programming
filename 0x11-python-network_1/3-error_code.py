54d948154153a9115ad8fcb125e14559febdbc34
omeinsotelo <omeinsotelo@gmail.com>
2020-04-15 23:55:12 -0500

    upload 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Sends a request to the URL and displays the body of the response 
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
