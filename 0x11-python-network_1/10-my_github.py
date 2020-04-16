f5658cfa9170b59f9d2955e9508bdb4278d5d2f8
omeinsotelo <omeinsotelo@gmail.com>
2020-04-16 01:12:38 -0500

    upload 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Takes your Github credentials and uses the Github API to display your id
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(r.json().get("id"))
