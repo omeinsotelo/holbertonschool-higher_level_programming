7eda45aa8223f554037e86dd07323d2bded6a011
omeinsotelo <omeinsotelo@gmail.com>
2020-04-15 21:47:46 -0500

    fixin 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
- #!/usr/bin/python3
- # Python script that fetches https://intranet.hbtn.io/status
- 
- if __name__ == "__main__":
-    from urllib import request
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+#!/usr/bin/python3
+# Script that fetches https://intranet.hbtn.io/status
+import urllib.request
 
-    print("Body response:")
-    print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
+
+if __name__ == '__main__':
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as reponse:
+        html = reponse.read()
+        print("Body response:")
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
           format(type(html), html, html.decode('utf-8')))
