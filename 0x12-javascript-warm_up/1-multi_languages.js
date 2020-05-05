16e19d5286159a4c963e7de7ec540b46c18b33e5
omeinsotelo <omeinsotelo@gmail.com>
2020-05-05 17:59:43 -0500

    upload 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+Script that prints 3 lines
+*/
+const myVarInC = 'C is fun';
+const myVarInP = 'Python is cool';
+const myVarInJs = 'Javascript is amazing';
+console.log(myVarInC + '\n' + myVarInP + '\n' + myVarInJs);
