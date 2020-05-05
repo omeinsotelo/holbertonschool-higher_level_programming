92033430cc4b9ebbb0568d7177d15ceaad09f5b3
omeinsotelo <omeinsotelo@gmail.com>
2020-05-05 18:54:16 -0500

    upload task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+Script prints two arguments passed to it, in the following format
+*/
+const myVar1 = process.argv[2];
+const myVar2 = process.argv[3];
+console.log(myVar1 + ' is ' + myVar2);
