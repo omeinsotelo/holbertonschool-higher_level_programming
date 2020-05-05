b7c6122777992d8cea73506f140443142d182f5a
omeinsotelo <omeinsotelo@gmail.com>
2020-05-05 18:27:09 -0500

    upload 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+Script prints a message depending number of arguments passed:
+*/
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
+}
