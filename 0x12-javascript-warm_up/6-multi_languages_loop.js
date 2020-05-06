fc6f16d98a282bc03fc8d75b149628a40371b9ee
omeinsotelo <omeinsotelo@gmail.com>
2020-05-05 19:51:41 -0500

    upload 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+Script that prints 3 lines using an array of string and a loop
+*/
+const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+while (i < list.length) {
+  console.log(list[i]);
+  i++;
+}
