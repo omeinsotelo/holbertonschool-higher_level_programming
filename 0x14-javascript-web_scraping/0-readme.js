906f68d5412ccf8fe7575a18fad21bb7fc37a438
omeinsotelo <omeinsotelo@gmail.com>
2020-05-12 18:55:31 -0500

    upload 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+Script that reads and prints the content of a file
+*/
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', function (error, data) {
+  console.log(error || data);
+});
