b711a6d057438f9afb5ac7b21570833b29e7dcf8
omeinsotelo <omeinsotelo@gmail.com>
2020-05-12 19:09:50 -0500

    upload 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+Script that writes a string to a file.
+*/
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], function (error) {
+  if (error) console.log(error);
+});
